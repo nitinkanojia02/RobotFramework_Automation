fb47b8defa1fcaeafee75dcb5974a933d254eeeb
ANURAG <anurag6185@gmail.com>
2025-05-25 10:22:41 +0530

    push heroku work

--- Tests/herokuapp_tests_variables.py
+++ Tests/herokuapp_tests_variables.py
@@ -6,4 +6,13 @@ a_element_locator= "id:column-a"
 b_element_locator= "id:column-b"
 elements_header= "//div[@class='column']/header"
 
-abc = 'qwe'
+AB_Testing_locator= "//a[text()='A/B Testing']"
+AB_Testing_heading_locator= "//h3[text()='A/B Test Variation 1']"
+AB_Testing_location_string= "abtest"
+
+Add_Remove_Elements_locator= "//a[text()='Add/Remove Elements']"
+Add_Remove_Elements_location_string= "add_remove_elements/"
+Add_Remove_Elements_heading_locator= "//h3[text()='Add/Remove Elements']"
+Add_Elements_locator= "//button[text()='Add Element']"
+Delete_Elements_locator= "//button[text()='Delete']"
+
